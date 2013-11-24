0500dfccf61697a801562ff180f1e9e5f1025774
Toralf Wittner <tw@dtex.org>
2013-11-24 13:10:05 +0100

    issue #6: Reset timeout on incoming data.
    Even if no pong has been received, the fact that we read other incoming
    data is taken as evidence that the connection is healthy and thus we
    reset the timeout.

--- src/Network/WebSockets/Snap.hs
+++ src/Network/WebSockets/Snap.hs
@@ -54,13 +54,18 @@ instance Exception ServerAppDone where
 
 
 --------------------------------------------------------------------------------
-copyIterateeToMVar :: MVar Chunk -> E.Iteratee ByteString IO ()
-copyIterateeToMVar mvar = E.catchError go handler
+copyIterateeToMVar :: ((Int -> Int) -> IO ())
+                   -> MVar Chunk
+                   -> E.Iteratee ByteString IO ()
+copyIterateeToMVar tickle mvar = E.catchError go handler
   where
     go = do
         mbs <- EL.head
         case mbs of
-            Just x  -> lift (putMVar mvar (Chunk x)) >> go
+            Just x  -> do
+                lift (tickle (max 60))
+                lift (putMVar mvar (Chunk x))
+                go
             Nothing -> lift (putMVar mvar Eof)
 
     handler se@(SomeException e) = case cast e of
@@ -142,7 +147,7 @@ runWebSocketsSnapWith options app = do
                     }
 
         _ <- lift $ forkIO $ app pc >> throwTo thisThread ServerAppDone
-        copyIterateeToMVar mvar
+        copyIterateeToMVar tickle mvar
 
 
 --------------------------------------------------------------------------------
