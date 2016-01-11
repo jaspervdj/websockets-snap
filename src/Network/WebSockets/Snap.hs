cad2c29ac2f6f12581e221a611574c66452fc960
Greg Hale <imalsogreg@gmail.com>
2016-01-10 23:27:06 -0500

    Continuing to debug
     - Add proper io-streams debugging
     - Change the call to WS.makeStream
     - Add forkPingThread function

--- src/Network/WebSockets/Snap.hs
+++ src/Network/WebSockets/Snap.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-} -- TODO for testing
 {-# LANGUAGE FlexibleContexts #-}  -- TODO for testing
 
 --------------------------------------------------------------------------------
@@ -12,18 +13,21 @@ module Network.WebSockets.Snap
 --------------------------------------------------------------------------------
 import           Blaze.ByteString.Builder      (Builder)
 import qualified Blaze.ByteString.Builder      as Builder
-import           Control.Concurrent            (forkIO, myThreadId)
+import           Control.Concurrent            (forkIO, myThreadId, threadDelay)
 import           Control.Concurrent.MVar       (MVar, newEmptyMVar, putMVar,
                                                 takeMVar)
 import           Control.Exception             (Exception (..),
-                                                SomeException (..), throw,
-                                                throwTo)
+                                                SomeException (..), handle,
+                                                throw, throwTo)
+import           Control.Monad                 (forever)
+import           Control.Monad.IO.Class        (liftIO)
 import           Control.Monad.Trans           (lift)
+import           Data.Bool                     (bool)
 import           Data.ByteString               (ByteString)
+import qualified Data.ByteString.Char8         as BC
 import qualified Data.ByteString.Builder       as BSBuilder
 import qualified Data.ByteString.Builder.Extra as BSBuilder
 import qualified Data.ByteString.Lazy          as BL
---import qualified Data.Enumerator               as E
 import           Data.IORef                    (newIORef, readIORef, writeIORef)
 import           Data.Monoid                   ((<>))
 import           Data.Typeable                 (Typeable, cast)
@@ -33,9 +37,13 @@ import qualified Network.WebSockets.Stream     as WS
 import qualified Snap.Core                     as Snap
 import qualified Snap.Internal.Http.Types      as Snap
 import qualified Snap.Types.Headers            as Headers
+import           System.IO                     (hFlush, stdout)
 import           System.IO.Streams             (InputStream, OutputStream)
 import qualified System.IO.Streams             as Streams
+import Data.ByteString.Builder.Extra           (flush)
 import qualified System.IO.Streams.Combinators as Streams
+import qualified System.IO.Streams.Handle      as Streams
+import qualified System.IO.Streams.Debug       as Streams
 
 
 --------------------------------------------------------------------------------
@@ -67,6 +75,9 @@ runWebSocketsSnap :: WS.ServerApp
 runWebSocketsSnap = runWebSocketsSnapWith WS.defaultConnectionOptions
 
 
+dropEmpty :: String -> Maybe String
+dropEmpty s = bool (Just s) Nothing (null s)
+
 --------------------------------------------------------------------------------
 -- | Variant of 'runWebSocketsSnap' which allows custom options
 runWebSocketsSnapWith :: WS.ConnectionOptions
@@ -75,38 +86,51 @@ runWebSocketsSnapWith :: WS.ConnectionOptions
 runWebSocketsSnapWith options app = do
   rq <- Snap.getRequest
   Snap.escapeHttp $ \tickle readEnd writeEnd -> do
+    let tickle' f = print "(calling Tickle)" >> tickle f
+    readEnd'  <- Streams.lockingInputStream  readEnd
+    writeEnd' <- Streams.lockingOutputStream writeEnd
 
-    let debugRead s = do
-          putStrLn "About to read"
-          r <- Streams.read s
-          putStrLn $ "Read " ++ show r
-          return r
-
-    let debugWrite v s = do
-          putStrLn $ "About to write " ++ show (BSBuilder.toLazyByteString <$> v)
-          r <- Streams.write v s
-          putStrLn $ "Wrote"
+    readEnd'' <- Streams.debugInput id "InputStream: " Streams.stdout readEnd'
+    writeEnd'' <- Streams.debugOutput (BL.toStrict . BSBuilder.toLazyByteString) "OutputStream: " Streams.stdout writeEnd'
 
     thisThread <- myThreadId
-    stream <- WS.makeStream
-              (Streams.read readEnd)
-              (\v ->
-                 Streams.write (((<> BSBuilder.flush) . BSBuilder.lazyByteString) <$> v) writeEnd
-              )
+    stream <- WS.makeStream (Streams.read readEnd'')
+              (\v -> Streams.write (fmap BSBuilder.lazyByteString v) writeEnd'' >>
+                     Streams.write (Just flush) writeEnd'')
+    liftIO $ print "Done making stream"
 
     let options' = options
                    { WS.connectionOnPong = do
-                        tickle (max 30)
+                        print "GOT A PONG"
+                        hFlush stdout
+                        tickle' (max 30)
                         WS.connectionOnPong options
                    }
 
         pc = WS.PendingConnection
-               { WS.pendingOptions = options'
-               , WS.pendingRequest = fromSnapRequest rq
-               , WS.pendingOnAccept = \_ -> return ()
+               { WS.pendingOptions  = options'
+               , WS.pendingRequest  = fromSnapRequest rq
+               , WS.pendingOnAccept = forkPingThread tickle'
                , WS.pendingStream   = stream
                }
-    app pc >> throwTo thisThread ServerAppDone
+    app pc >> print "app exited" >> throwTo thisThread ServerAppDone
+
+
+--------------------------------------------------------------------------------
+-- | Start a ping thread in the background
+forkPingThread :: ((Int -> Int) -> IO ()) -> WS.Connection -> IO ()
+forkPingThread tickle conn = do
+    _ <- forkIO pingThread
+    return ()
+  where
+    pingThread = handle ignore $ forever $ do
+        WS.sendPing conn (BC.pack "ping")
+        print "TICKLING"
+        tickle (min 30)
+        threadDelay $ 5 * 1000 * 1000
+
+    ignore :: SomeException -> IO ()
+    ignore _   = return ()
 
 
 --------------------------------------------------------------------------------
@@ -117,5 +141,3 @@ fromSnapRequest rq = WS.RequestHead
     , WS.requestHeaders = Headers.toList (Snap.rqHeaders rq)
     , WS.requestSecure  = Snap.rqIsSecure rq
     }
-
-    
