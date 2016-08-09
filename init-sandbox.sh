aeec17239979356ade7e6090b32f2b2062b72f15
Greg Hale <imalsogreg@gmail.com>
2016-08-08 21:01:41 -0400

    Drop submodules for snap 1.0

--- init-sandbox.sh
+++
@@ -1,8 +0,0 @@
-cabal sandbox init
-cabal sandbox add-source deps/snap
-cabal sandbox add-source deps/snap/deps/io-streams
-cabal sandbox add-source deps/snap/deps/xmlhtml
-cabal sandbox add-source deps/snap/deps/snap-core
-cabal sandbox add-source deps/snap/deps/snap-server
-cabal sandbox add-source deps/snap/deps/io-streams-haproxy
-cabal sandbox add-source deps/snap/deps/heist
